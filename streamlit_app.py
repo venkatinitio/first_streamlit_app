0c8f5d7ad92cbe21b2f24cc6fa123600c312df32
venkatinitio <30770272+venkatinitio@users.noreply.github.com>
2023-07-17 15:38:18 +0530

    Update streamlit_app.py
    Title and Header Change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Moms New Healthy Diner')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 and Blueberry Oat Meal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard Boiled Free-Range Egg')
