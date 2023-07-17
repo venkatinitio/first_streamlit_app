8f12d643d6060fe7a423f6f08e4db45a90c48fbd
venkatinitio <30770272+venkatinitio@users.noreply.github.com>
2023-07-17 15:53:22 +0530

    Update streamlit_app.py
    add picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build your own smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.multiselect("Pick Some Fruits: ", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
