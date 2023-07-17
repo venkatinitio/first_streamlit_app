8d32376bcff281128855c8e5ed8432c49397429a
venkatinitio <30770272+venkatinitio@users.noreply.github.com>
2023-07-17 15:40:18 +0530

    Update streamlit_app.py
    apostrophy s experiment 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 and Blueberry Oat Meal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
