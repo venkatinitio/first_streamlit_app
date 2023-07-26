2d3ba17e3c2ce182ee1a36cf3a0f7dcd7068c21a
venkatinitio <30770272+venkatinitio@users.noreply.github.com>
2023-07-26 13:05:16 +0530

    Update streamlit_app.py
    pulling all the fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The FruitLoad List Contains: ")
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
